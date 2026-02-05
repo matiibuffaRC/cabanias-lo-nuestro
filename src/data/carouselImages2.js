1705bea22abddc92df17bc4ab3283d91c9312f27
matiibuffaRC <matbuffautn05@gmail.com>
2026-02-05 15:33:23 -0300

    ...

--- src/data/carouselImages2.js
+++ src/data/carouselImages2.js
@@ -1,6 +1,6 @@
 import imagen1 from "../imgs/cartel.jpg";
 import imagen2 from "../imgs/cabania.jpg";
-import imagen3 from "../imgs/patiobien.jpg";
+import imagen3 from "../imgs/patioBien.jpg";
 import imagen4 from "../imgs/patio.jpg";
 import imagen6 from "../imgs/frente.jpg";
 import imagen7 from "../imgs/cochera.jpg";
