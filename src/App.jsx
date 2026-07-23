f5305bef2a7469b163f4fbd610e37ba5456a0cf0
matiibuffaRC <matbuffautn05@gmail.com>
2026-07-23 10:06:04 -0300

    Ver comentarios del app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter ,Routes, Route } from "react-router-dom";
 import ScrollToTop from "../src/Components/ScrollToTop/ScrollToTopComponent";
 
 // Modularizar componente del botton de whatsapp y mejorar estructura de todos los componentes, para que sean más legibles y fáciles de mantener.
-
+// Ver cuestión de los íconos de cada opción y mejorar cuestión de las imágenes (en calidad)
 
 // Importamos las pages
 
